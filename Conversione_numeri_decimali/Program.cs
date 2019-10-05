bd2125add9bb5a03fa4487759617a332fd6ea3e5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:29:12 +0200

    Add project files.

---
+++ Conversione_numeri_decimali/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conversione_numeri_decimali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
